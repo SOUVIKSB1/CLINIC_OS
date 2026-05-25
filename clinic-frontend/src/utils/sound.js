bf5ba64a55b4363149f8dc1c24f24e71be098d20
Souvik Sinhababu <souviksinhababu1@gmail.com>
2026-05-25 21:22:29 +0530

    sound

--- clinic-frontend/src/utils/sound.js
+++ clinic-frontend/src/utils/sound.js
@@ -1,5 +1,5 @@
 const sounds = {
-  success: new Audio("/sounds/success.wav"),
+  success: new Audio("/sounds/success.mp3"),
   appointment: new Audio("/sounds/appointment.mp3"),
   notification: new Audio("/sounds/notification.mp3"),
 };
